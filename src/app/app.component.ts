b7b3d17803a0fb27ad2b51b1d638cd18e76f3d08
Adnan <adnanghannam21@gmail.com>
2023-09-18 09:32:07 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'docs-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'documentation-app';
-}
+export class AppComponent { }
