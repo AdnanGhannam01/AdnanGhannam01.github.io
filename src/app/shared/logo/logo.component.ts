afe02425be18605efe1a33f81310fa2266cf010a
Adnan <adnanghannam21@gmail.com>
2023-09-28 20:57:05 +0300

    fix logo

--- src/app/shared/logo/logo.component.ts
+++ src/app/shared/logo/logo.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: '.docs-logo',
+  host: {
+    "class": "docs-link"
+  },
   templateUrl: './logo.component.html',
   styleUrls: ['./logo.component.scss']
 })
